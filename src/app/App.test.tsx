39aa32b94c24730aaac989cf34bc58f9547f01bb
tushermahmud <sazzadmahmud01795355849@gmail.com>
2023-12-04 01:18:07 +0600

    fix app test

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -4,6 +4,6 @@ import { App } from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Score 200 points/i);
   expect(linkElement).toBeInTheDocument();
 });
